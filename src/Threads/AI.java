7f01a4c404f0e7f08eaeb5ba8ec7de4a50c3099f
Tony Cunsolo <t27302013@correo.unimet.edu.ve>
2023-11-05 21:54:48 -0400

    Agregando reglas del combate
    Realizando los metodos para que la batalla se realice y se tenga un ganador, un empate o reparacion

--- src/Threads/AI.java
+++ src/Threads/AI.java
@@ -8,6 +8,20 @@ package Threads;
 import DB.Buffer;
 import Character.Character;
 import Enums.CharacterTypeEnum;
+import static Enums.CharacterTypeEnum.AIR;
+import static Enums.CharacterTypeEnum.ARMONY;
+import static Enums.CharacterTypeEnum.CHAOS;
+import static Enums.CharacterTypeEnum.DARKNESS;
+import static Enums.CharacterTypeEnum.EARTH;
+import static Enums.CharacterTypeEnum.FIRE;
+import static Enums.CharacterTypeEnum.ICE;
+import static Enums.CharacterTypeEnum.LIGHT;
+import static Enums.CharacterTypeEnum.MAGMA;
+import static Enums.CharacterTypeEnum.NORMAL;
+import static Enums.CharacterTypeEnum.STEEL;
+import static Enums.CharacterTypeEnum.THUNDER;
+import static Enums.CharacterTypeEnum.WATER;
+import Enums.TierEnum;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,88 +44,169 @@ public class AI extends Thread{
             Character capcom = buffer.getCapcomFighter();
             Character nintendo = buffer.getNintendoFighter();
             
-            CharacterTypeEnum [] capcomBuffs = getGoodAgainst(capcom.getCharacterType());
-            CharacterTypeEnum [] nintendoBuffs = getGoodAgainst(nintendo.getCharacterType());
+            sleep(1000);
+            
+            if(capcom != null && nintendo != null){
+                
+                int randNum = (int) Math.random() * 100;
+
+                if(randNum < 40){
+
+                    int capcomBuffs = getBuffBonus(capcom.getCharacterType(), nintendo.getCharacterType());
+                    capcom.setPower(capcomBuffs+capcom.getPower());
+
+                    int nintendoBuffs = getBuffBonus(nintendo.getCharacterType(), capcom.getCharacterType());
+                    nintendo.setPower(nintendoBuffs+nintendo.getPower());
+                    
+                    if(nintendo.getPower() > capcom.getPower())
+                        buffer.getCapcomWinningQueue().queue(nintendo);
+                    else
+                        buffer.getCapcomWinningQueue().queue(capcom);
+                }
+
+                else if(randNum < 67){
+                    
+                    getToTierQueue(capcom);
+                    getToTierQueue(nintendo);
+                }
+
+                else{
+                    
+                    capcom.setTier(TierEnum.FIX);
+                    getToTierQueue(capcom);
+                    nintendo.setTier(TierEnum.FIX);
+                    getToTierQueue(nintendo);
+                }
+            }
             
         } catch (InterruptedException ex) {
             Logger.getLogger(Administrator.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public CharacterTypeEnum [] getGoodAgainst(CharacterTypeEnum typeEnum) {
+    private int getBuffBonus(CharacterTypeEnum characterType, CharacterTypeEnum enemyType) {
     
-        CharacterTypeEnum [] temp =  new CharacterTypeEnum[3];
+        CharacterTypeEnum [] temp = getGoodAgainst(characterType);
         
-        switch(typeEnum){
-            case AIR:
-                temp[0] = CharacterTypeEnum.FIRE;
-                temp[1] = CharacterTypeEnum.DARKNESS;
-                temp[2] = null;
-                break;
-            case WATER:
-                temp[0] = CharacterTypeEnum.EARTH;
-                temp[1] = CharacterTypeEnum.LIGHT;
-                temp[2] = null;
-                break;
-            case EARTH:
-                temp[0] = CharacterTypeEnum.LIGHT;
-                temp[1] = CharacterTypeEnum.AIR;
-                temp[2] = null;
-                break;
-            case FIRE:
-                temp[0] = CharacterTypeEnum.DARKNESS;
-                temp[1] = CharacterTypeEnum.WATER;
-                temp[2] = null;
-                break;
-            case LIGHT:
-                temp[0] = CharacterTypeEnum.AIR;
-                temp[1] = CharacterTypeEnum.FIRE;
-                temp[2] = null;
-                break;
-            case DARKNESS:
-                temp[0] = CharacterTypeEnum.WATER;
-                temp[1] = CharacterTypeEnum.EARTH;
-                temp[2] = null;
-                break;
-            case THUNDER:
-                temp[0] = CharacterTypeEnum.FIRE;
-                temp[1] = CharacterTypeEnum.DARKNESS;
-                temp[2] = CharacterTypeEnum.WATER;
-                break;
-            case ICE:
-                temp[0] = CharacterTypeEnum.EARTH;
-                temp[1] = CharacterTypeEnum.LIGHT;
-                temp[2] = CharacterTypeEnum.AIR;
-                break;
-            case STEEL:
-                temp[0] = CharacterTypeEnum.LIGHT;
-                temp[1] = CharacterTypeEnum.AIR;
-                temp[2] = CharacterTypeEnum.FIRE;
-                break;
-            case MAGMA:
-                temp[0] = CharacterTypeEnum.DARKNESS;
-                temp[1] = CharacterTypeEnum.WATER;
-                temp[2] = CharacterTypeEnum.EARTH;
-                break;
-            case ARMONY:
-                temp[0] = CharacterTypeEnum.AIR;
-                temp[1] = CharacterTypeEnum.FIRE;
-                temp[2] = CharacterTypeEnum.DARKNESS;
-                break;
-            case CHAOS:
-                temp[0] = CharacterTypeEnum.WATER;
-                temp[1] = CharacterTypeEnum.EARTH;
-                temp[2] = CharacterTypeEnum.LIGHT;
-                break;
-            case NORMAL:
-                temp[0] = null;
-                temp[1] = null;
-                temp[2] = null;
-                break;
-            default:
-                throw new AssertionError(typeEnum.name());
+        int indexBuff = 0;
+        int isNotNull = 0;
+        int i =0;
+        boolean buffFound = false;
+        
+        for(CharacterTypeEnum type :temp){
+            
+            if(type != null)
+                isNotNull++;
+            
+            if(type == enemyType && !buffFound){
+                indexBuff = i;
+                buffFound = true;
+            }
+            else
+                i++;
         }
         
-            return temp;
+        if(buffFound)
+            return isNotNull-indexBuff;
+        else
+            return 0;
+    }
+
+    private CharacterTypeEnum[] getGoodAgainst(CharacterTypeEnum characterType) {
+        
+         CharacterTypeEnum [] goodAgainst =  new CharacterTypeEnum[3];
+        
+        switch(characterType){
+            case AIR:
+                goodAgainst[0] = CharacterTypeEnum.FIRE;
+                goodAgainst[1] = CharacterTypeEnum.DARKNESS;
+                goodAgainst[2] = null;
+                break;
+            case WATER:
+                goodAgainst[0] = CharacterTypeEnum.EARTH;
+                goodAgainst[1] = CharacterTypeEnum.LIGHT;
+                goodAgainst[2] = null;
+                break;
+            case EARTH:
+                goodAgainst[0] = CharacterTypeEnum.LIGHT;
+                goodAgainst[1] = CharacterTypeEnum.AIR;
+                goodAgainst[2] = null;
+                break;
+            case FIRE:
+                goodAgainst[0] = CharacterTypeEnum.DARKNESS;
+                goodAgainst[1] = CharacterTypeEnum.WATER;
+                goodAgainst[2] = null;
+                break;
+            case LIGHT:
+                goodAgainst[0] = CharacterTypeEnum.AIR;
+                goodAgainst[1] = CharacterTypeEnum.FIRE;
+                goodAgainst[2] = null;
+                break;
+            case DARKNESS:
+                goodAgainst[0] = CharacterTypeEnum.WATER;
+                goodAgainst[1] = CharacterTypeEnum.EARTH;
+                goodAgainst[2] = null;
+                break;
+            case THUNDER:
+                goodAgainst[0] = CharacterTypeEnum.FIRE;
+                goodAgainst[1] = CharacterTypeEnum.DARKNESS;
+                goodAgainst[2] = CharacterTypeEnum.WATER;
+                break;
+            case ICE:
+                goodAgainst[0] = CharacterTypeEnum.EARTH;
+                goodAgainst[1] = CharacterTypeEnum.LIGHT;
+                goodAgainst[2] = CharacterTypeEnum.AIR;
+                break;
+            case STEEL:
+                goodAgainst[0] = CharacterTypeEnum.LIGHT;
+                goodAgainst[1] = CharacterTypeEnum.AIR;
+                goodAgainst[2] = CharacterTypeEnum.FIRE;
+                break;
+            case MAGMA:
+                goodAgainst[0] = CharacterTypeEnum.DARKNESS;
+                goodAgainst[1] = CharacterTypeEnum.WATER;
+                goodAgainst[2] = CharacterTypeEnum.EARTH;
+                break;
+            case ARMONY:
+                goodAgainst[0] = CharacterTypeEnum.AIR;
+                goodAgainst[1] = CharacterTypeEnum.FIRE;
+                goodAgainst[2] = CharacterTypeEnum.DARKNESS;
+                break;
+            case CHAOS:
+                goodAgainst[0] = CharacterTypeEnum.WATER;
+                goodAgainst[1] = CharacterTypeEnum.EARTH;
+                goodAgainst[2] = CharacterTypeEnum.LIGHT;
+                break;
+            case NORMAL:
+                goodAgainst[0] = null;
+                goodAgainst[1] = null;
+                goodAgainst[2] = null;
+                break;
+            default:
+                throw new AssertionError(characterType.name());
+        }
+        
+        return goodAgainst;
+    }
+
+    private void getToTierQueue(Character ch) {
+        
+        switch(ch.getTier()){
+            
+            case WEAK:
+                buffer.getCapcomQueue3().queue(ch);
+                break;
+            case NORMAL:
+                buffer.getCapcomQueue2().queue(ch);
+                break;
+            case STRONG:
+                buffer.getCapcomQueue1().queue(ch);
+                break;
+            case FIX:
+                buffer.getCapcomQueue4().queue(ch);
+                break;
+            default:
+                throw new AssertionError(ch.getTier().name());
+        }
     }
 }
